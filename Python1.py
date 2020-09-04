2cef200cfb4d43e1749837fcec1855fa5fb1c17a
Towa0208 <Towa0208angerme@outlook.jp>
2020-09-05 00:23:46 +0900

    Progate Python1

--- Python1.py
+++ Python1.py
@@ -1,2 +1,16 @@
-print("Hello World")
-print("こんにちは")
+money=10000
+cd=1667
+count=input("cdを何枚買いますか?")
+
+total_cd=cd*int(count)
+
+total_cost=money-total_cd
+
+print("CDを"+str(count)+"枚買いました")
+
+if(int(total_cost)>0):
+    print("CDを買って"+str(total_cost)+"円余りました")
+elif(total_cost==money):
+    print("ピッタリでした")
+elif(0>total_cost):
+    print("買えませんでした")
