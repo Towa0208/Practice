e741b5e1b14f7bbc3df6f0e6e468989b5bcbc011
Towa0208 <Towa0208angerme@outlook.jp>
2020-09-09 21:04:50 +0900

    モーダルテスト

---
+++ mordal/js/practice.js
@@ -0,0 +1,13 @@
+$(function(){
+    $("header").click(
+        function(){
+            $(".content").fadeIn();
+        });
+    
+    $(".batu").click(
+        function(){
+            $(".content").fadeOut();
+        }
+    )
+    }
+);
